b69b3dc9697c881fc2bc7a720a316580c651baec
Matthew Toffey <mjtoffey@gmail.com>
2023-06-01 20:10:49 +0000

    added button

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-let storyText = It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-let insertX = ["Willy the Goblin" , "Big Daddy" , "Father Christmas"]
-let insertY = ["the soup kitchen" , "Disneyland" , "the White House"]
-let insertZ = ["spontaneously combusted" , "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+let storyText = It was 94 fahrenheit outside, so : insertx: went for a walk.When they got to: inserty:, they stared in horror for a few moments, then : insertz:.Bob saw the whole thing, but was not surprised — : insertx: weighs 300 pounds, and it was a hot day.
+let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
+let inserty = ["the soup kitchen", "Disneyland", "the White House"]
+let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
 
 randomize.addEventListener('click', result);
 
 function result() {
 
-  if(customName.value !== '') {
+  if (customName.value !== '') {
     const name = customName.value;
 
   }
 
-  if(document.getElementById("uk").checked) {
+  if (document.getElementById("uk").checked) {
     const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const temperature = Math.round(94);
 
   }
 
@@ -30,7 +30,16 @@ function result() {
   story.style.visibility = 'visible';
 }
 
-let newStory = storyText
-let xItem = randomValueFromArray(insertX)
-let yItem = randomValueFromArray(insertY)
-let zItem = randomValueFromArray(insertZ)
+let newStory = storyText;
+let xItem = randomValueFromArray(insertx);
+let yItem = randomValueFromArray(inserty);
+let zItem = randomValueFromArray(insertz);
+
+function newStory(buttonPress); {
+  content.contentString.replace("xItem ", "yItem ", "zItem");
+  newStory.contentString.replace()
+}
+var input = document.getElementById("customname");
+if (input !== input.value) {
+  customname = 'Bob';
+} 
