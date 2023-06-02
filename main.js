66c0d76d2990052422f8df956e6cf0eea3fbbc8a
Matthew Toffey <mjtoffey@gmail.com>
2023-06-02 00:36:29 +0000

    fixed errors

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+let customName = document.getElementById('customname');
+let randomize = document.querySelector('.randomize');
+let story = document.querySelector('.story');
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
+  let random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-const storyText = It was 94 fahrenheit outside, so : insertx: went for a walk.When they got to: inserty:, they stared in horror for a few moments, then : insertz:.Bob saw the whole thing, but was not surprised — : insertx: weighs 300 pounds, and it was a hot day.
-const insertX = [
+let storyText = "It was 94 fahrenheit outside, so : insertx: went for a walk.When they got to: inserty:, they stared in horror for a few moments, then : insertz:.Bob saw the whole thing, but was not surprised — : insertx: weighs 300 pounds, and it was a hot day."
+let insertX = [
   "Willy the Goblin",
   "Big Daddy",
   "Father Christmas"
 ]
-const insertX = [
+let insertY = [
   "the soup kitchen",
   "Disneyland",
   "the White House"
 ]
-const insertZ = [
+let insertZ = [
   "spontaneously combusted",
   "melted into a puddle on the sidewalk",
   "turned into a slug and crawled away"
